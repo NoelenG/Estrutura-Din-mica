ca2d9ffc1f0d063fe20c21d4da579926399b0b7d
Noelen Giovanna <Noelen.giovanna@conam.com.br>
2020-04-07 21:09:51 -0300

    my first commit

---
+++ src/test/java/br/noelen/dinamicTest.java
@@ -0,0 +1,18 @@
+package br.noelen;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class dinamicTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
